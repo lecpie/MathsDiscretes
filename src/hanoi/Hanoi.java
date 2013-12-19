eb6d5eb3b3ed4c08f2e3836e76b0b336f173ff69
lemessager <quedupur@hotmail.fr>
2013-12-19 09:16:28 +0100

    j'ai modifié la taille par défaut

--- src/hanoi/Hanoi.java
+++ src/hanoi/Hanoi.java
@@ -107,7 +107,7 @@ public class Hanoi {
 	 * @param args : number of tower to use, default is 3
 	 */
 	public static void main (String[] args) {
-		int size = 3;
+		int size = 12;
 		if (args.length == 1)
 			size = Integer.parseInt(args[0]);
 		
